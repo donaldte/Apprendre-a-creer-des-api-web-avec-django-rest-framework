7c4eeeb4a307608155f1663f6a3718a069c043e5
donaldte <donaldtedom0@gmail.com>
2022-05-27 08:04:51 +0100

    votre premier api

--- DonaldproAPI/urls.py
+++ DonaldproAPI/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
+
+endpoint = 'http://127.0.0.1:8081/api'
