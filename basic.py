3cc1704dac3c7652cfd163ffcebbbd9e9689eebe
donaldte <donaldtedom0@gmail.com>
2022-05-28 11:20:56 +0100

    suite partie5

--- basic.py
+++ basic.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint = "http://127.0.0.1:8081/api/"
-response = requests.get(endpoint)
+endpoint = "http://127.0.0.1:8000/api/"
+response = requests.get(endpoint, params={'abc':123}, json={'query':'Hello'})
 print(response.json())
 print(response.status_code)
 
